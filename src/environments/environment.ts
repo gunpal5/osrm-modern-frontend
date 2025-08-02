5220ca8e1b8b8e65761ca3548c03b041729c1884
Gunpal Jain <gunpal5@hotmail.com>
2025-08-02 15:04:09 +0530

    Update OSRM URL protocol from HTTPS to HTTP in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  osrmUrl: 'https://localhost:5000',
+  osrmUrl: 'http://localhost:5000',
   nominatimUrl: 'https://nominatim.openstreetmap.org'
 };
